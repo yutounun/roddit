fa59e30772cef79b50e2d5cec4bcbb09da5bd449
yutounun <geek.yuto@gmail.com>
2024-04-06 15:38:02 +0700

    setup next-auth

---
+++ src/auth.ts
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth";
+import Github from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { db } from "@/db/index";
+
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
+  throw new Error("Missing GITHUB_CLIENT_ID or GITHUB_CLIENT_SECRET");
+}
+
+export const {
+  header: { GET, POST },
+  auth,
+  signOut,
+  signIn,
+} = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [
+    Github({
+      clientId: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    // usually not needed, but just for fixing a bug in nextauth
+    async session({ session, user }: any) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+});
