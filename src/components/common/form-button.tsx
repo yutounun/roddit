9851da85715a1662f124a269128622b9d4cfed45
yutounun <geek.yuto@gmail.com>
2024-04-08 09:34:29 +0700

    complete topic create function

---
+++ src/components/common/form-button.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+import { Button } from "@nextui-org/react";
+
+interface FormButtonProps {
+  children: React.ReactNode;
+}
+
+function FormButton({ children }: FormButtonProps) {
+  const { pending } = useFormStatus();
+  return (
+    <Button type="submit" isLoading={pending}>
+      {children}
+    </Button>
+  );
+}
+
+export default FormButton;
