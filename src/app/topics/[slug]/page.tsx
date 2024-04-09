0b5142a723b49ddcd11388c652d5f6bfe8ab1b8c
yutounun <geek.yuto@gmail.com>
2024-04-09 17:40:05 +0700

    complete topic page

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import CreatePostForm from "@/components/posts/post-create-form";
+import PostList from "@/components/posts/post-list";
+import { fetchPostsByTopicSlug } from "@/db/queries/posts";
 
 type TopicShowPageProps = {
   params: {
@@ -12,6 +14,7 @@ function TopicShowPage({ params }: TopicShowPageProps) {
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+        <PostList fetchData={() => fetchPostsByTopicSlug(slug)} />
       </div>
       <div>
         <CreatePostForm slug={slug} />
---
+++ src/db/queries/posts.ts
@@ -0,0 +1,35 @@
+import type { Post } from "@prisma/client";
+import { db } from "@/db/index";
+
+export type PostWithData = Post & {
+  topic: { slug: string };
+  user: { name: string | null };
+  _count: { comments: number };
+};
+
+export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
+  return db.post.findMany({
+    where: {
+      topic: {
+        slug,
+      },
+    },
+    include: {
+      topic: {
+        select: {
+          slug: true,
+        },
+      },
+      user: {
+        select: {
+          name: true,
+        },
+      },
+      _count: {
+        select: {
+          comments: true,
+        },
+      },
+    },
+  });
+}
